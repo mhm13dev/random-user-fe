ef6d9a3f31ccf18e31d7578eb98aedd2f118a97f
Mubashir Hassan <official.mhm13@gmail.com>
2024-03-09 15:45:25 +0500

    feat: added route progress bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import NextTopLoader from "nextjs-toploader";
 import Header from "@/components/header";
 import UsersProvider from "@/components/users-provider";
 import "./globals.css";
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <NextTopLoader color="#22c55e" height={3} showSpinner={false} />
         <Header />
         <UsersProvider>{children}</UsersProvider>
       </body>
